1b9e462ccd09f4f05b0c467490bb2591f00b39ab
Japrajah <70380700+Japrajah@users.noreply.github.com>
2022-01-06 22:08:48 +0700

    Update Addr.h

--- SteamOverlayHook_POC/Addr.h
+++ SteamOverlayHook_POC/Addr.h
@@ -4,7 +4,6 @@
 class Addr
 {
 public:
-	static inline uintptr_t base_address = *(uintptr_t*)(__readgsqword(0x60) + 0x10);
 	static inline uintptr_t pCamera = Utils::GetAbsoluteAddress(
 		Utils::sigscan(0, "48 8B 05 ? ? ? ? 0F 57 C0 0F 11 80 ? ? ? ? 0F 11 80"), //Refaddress
 		3, 7);
